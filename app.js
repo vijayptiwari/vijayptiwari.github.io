1203b85b429f3d18f7716291c393b9b4d413a805
vijayptiwari <vijayptiwari28@gmail.com>
2026-06-23 07:17:20 +0530

    checkpoint before checking out main

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require("express"),
-    path = require("path"),
-    bodyParser = require("body-parser"),
-    app = express();
+const express = require("express");
+const path = require("path");
 
+const app = express();
 const port = process.env.PORT || 3000;
+const root = __dirname;
 
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  express.static(root, {
+    index: "index.html",
+    extensions: ["html"],
+    etag: true,
+    fallthrough: true,
+  })
 );
-app.use(bodyParser.json());
 
-app.use(express.static(path.resolve(__dirname)));
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(root, "index.html"));
 });
 
-const server = app.listen(port, function () {
-    console.log("The server is running on http://localhost:" + port);
+app.listen(port, () => {
+  console.log(`The server is running on http://localhost:${port}`);
 });
